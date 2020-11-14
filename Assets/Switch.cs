f48d79177d6ed18534978c4fd32abd23d6c0e9d1
Andrei Serban <andreiserbanrazvan@gmail.com>
2020-11-14 14:42:19 +0200

    Initial commit

---
+++ Assets/Switch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public GameObject up;
+    public GameObject on;
+    public bool isUp;
+    public bool isOn;
+    public AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        up.SetActive(isUp);
+        on.SetActive(isOn);
+        if (isOn)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        isUp = !isUp;
+        isOn = !isOn;
+        up.SetActive(isUp);
+        on.SetActive(isOn);
+        audio.Play();
+    }
+}
